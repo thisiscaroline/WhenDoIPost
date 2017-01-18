3fb9679e96b00bab04689a0601e78b5d85a472be
Caroline <caroline.swinyer@gmail.com>
2017-01-18 02:15:10 -0500

    Added text labels to data points

--- post.py
+++ post.py
@@ -34,11 +34,11 @@ def main():
 		username = input("Enter a username:\n>> ")
 		tweetCount = input("Enter the number of tweets you'd like to analyze:\n>> ")
 	
-	print('\n')
+	# print('\n')
 	
 	# Create a graph
 	plt.axis([1,7,0,12])
-	plt.xlabel('Days')
+	plt.xlabel('Sunday     Monday     Tuesday     Wednesday     Thursday     Friday     Saturday')
 	plt.ylabel('Hours')
 	
 	# Variables for later use
@@ -61,24 +61,22 @@ def main():
 	# x: Day of the week; y: Time they tweeted
 	for i in range(0, len(tweetList)):
 			
-		print(tweetList[i].ctime(), end=": ")
-		
 		x = form_x(str(tweetList[i].ctime()))
-		y = form_y(tweetList[i])
+		y = form_y(tweetList[i], 'y')
 		
-		coord = str(x)+", "+str(y)
+		minLabel = "0"+str(tweetList[i].minute) if tweetList[i].minute < 10 else str(tweetList[i].minute)
+		timeLabel = str(form_y(tweetList[i], 'n'))+":"+minLabel
 		
 		# Set AM or PM colors
 		if tweetList[i].hour < 12:
 			clr = 'r'
-			print("\x1b[1;31m%s\x1b[0m" % coord)
+			plt.annotate(timeLabel+" AM", xy=(x,y), xytext=(x+0.15,y+0.15))
 		else:
 			clr = 'b'
-			print("\x1b[1;34m%s\x1b[0m" % coord)
+			plt.annotate(timeLabel+" PM", xy=(x,y), xytext=(x+0.15,y+0.15))
 		
 		# Plot the coordinates on the graph
-		plt.scatter(x, y, color=clr, marker='o', s=100)
-		# input() # Add if you want to see coordinates
+		plt.scatter(x, y, c=clr, s=75, marker='o', edgecolors='black')
 	
 	plt.show()
 		
@@ -104,9 +102,8 @@ def form_x(tweet):
 	}[tweet[:3]]
 
 # Converts 24h time to 12h, minutes to increments
-def form_y(tweet):
+def form_y(tweet, yn):
 	
-	# print(str(tweet.hour)+":"+str(tweet.minute))
 	hour, minutes = 0, 0
 	
 	# Mod the hour, if necessary
@@ -119,8 +116,15 @@ def form_y(tweet):
 	elif (tweet.hour > 12):		# PM hours
 		hour = (tweet.hour % 12)
 	
-	# Minutes as a fraction of the hour
-	minutes = round(tweet.minute/60,2)
+	# Plotting vs. aesthetic decision
+	if yn == 'y':
+		# Plot as a fraction of the hour
+		minutes = round(tweet.minute/60,2)
+	elif yn == 'n':
+		# Print the hour as text
+		if hour == 0:
+			return 12
+		return hour
 	
 	return (hour+minutes)
 			
